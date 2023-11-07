74c17d366a767fe6bf8b27d8b5cca38d6f140367
Kushal Chhetri <71422133+kushal-chhetri7@users.noreply.github.com>
2023-11-07 20:26:02 +0600

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,6 @@ import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
 import { ActivatedRoute } from '@angular/router';
 
-
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,9 +10,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class HomeComponent implements OnInit {
+
   foods: Foods[] = [];
 
-  constructor(private fs: FoodService, private route: ActivatedRoute) { }
+  constructor(private fs: FoodService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
