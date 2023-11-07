374168642c39117bb77d02670571d9fdcad84a4a
Kushal Chhetri <71422133+kushal-chhetri7@users.noreply.github.com>
2023-11-07 20:27:05 +0600

    Update star-rating.component.ts

--- src/app/star-rating/star-rating.component.ts
+++ src/app/star-rating/star-rating.component.ts
@@ -5,7 +5,9 @@ import { Component, Input } from '@angular/core';
   templateUrl: './star-rating.component.html',
   styleUrls: ['./star-rating.component.css']
 })
+
 export class StarRatingComponent {
+
   @Input()
   stars!: number;
 
