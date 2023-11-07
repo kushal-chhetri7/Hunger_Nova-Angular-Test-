9e45b77f439f52ff2563d2487fd3d6737dffed2a
Kushal Chhetri <71422133+kushal-chhetri7@users.noreply.github.com>
2023-11-07 20:25:33 +0600

    Update not-found.component.ts

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -5,15 +5,15 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
+
 export class NotFoundComponent implements OnInit {
+
   @Input() visible:boolean = false;
   @Input() notFoundMessage:string = 'Nothing found!';
   @Input() resetLinkText:string = 'Reset';
   @Input() resetLinkRoute:string = '/';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
